710e0c51a59fa45b740eaed06ef32d69c605455c
Developer <developer@heating-supply.com>
2026-05-23 15:21:34 +0800

    fix

---
+++ functions/api/[[catchall]].js
@@ -0,0 +1,17 @@
+﻿export async function onRequest(context) {
+  const { request } = context;
+  const url = new URL(request.url);
+  const targetUrl = 'https://hs-app-4ist.onrender.com' + url.pathname + url.search;
+
+  const response = await fetch(targetUrl, {
+    method: request.method,
+    headers: request.headers,
+    body: request.method !== 'GET' && request.method !== 'HEAD' ? request.body : undefined,
+  });
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: response.headers,
+  });
+}
