c460f4c2d621444fe9608e71537cb114bca85aeb
Developer <developer@heating-supply.com>
2026-05-23 15:06:37 +0800

    fix

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,6 +1,6 @@
 ﻿import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://hs-app-4ist.onrender.com' });
+const api = axios.create({ baseURL: '' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
--- server/src/index.js
+++ server/src/index.js
@@ -20,7 +20,13 @@ import backupRoutes from './routes/backup.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  maxAge: 86400
+}));
 app.use(express.json());
 app.use(requestLogger);
 
