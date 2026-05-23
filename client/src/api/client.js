6fb77043bc4114300fbac2a6b45957275050a665
Developer <developer@heating-supply.com>
2026-05-23 14:56:43 +0800

    fix

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+﻿import axios from 'axios';
 
-const api = axios.create({ baseURL: '' });
+const api = axios.create({ baseURL: 'https://hs-app-4ist.onrender.com' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = Bearer ;
   }
   return config;
 });
